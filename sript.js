56b4d31ceb803bc08736cf38ccd3683df4a92a47
Sielarz <34028988+Sielarz@users.noreply.github.com>
2017-12-12 12:28:20 +0100

    Update sript.js

--- sript.js
+++ sript.js
@@ -1,18 +1,15 @@
 function getTriangleArea(a, h) {
-var a,
-	h;
-if (a <= 0 || h <= 0) {
-	console.log('Nieprawidłowe dane');
-} else {
-	value = a * h / 2;
+    if (a <= 0 || h <= 0) {
+        console.log('Nieprawidłowe dane');
+    } else {
+        value = a * h / 2;
+    }
+    return;
 }
-return value;
-}
-getTriangleArea();
 
 var triangle1Area = getTriangleArea(10, 6),
-	triangle2Area = getTriangleArea(12, 4),
-	triangle3Area = getTriangleArea(8, 5);
+    triangle2Area = getTriangleArea(12, 4),
+    triangle3Area = getTriangleArea(8, 5);
 
 console.log(triangle1Area);
 console.log(triangle2Area);
